93fda86e920012884e2f9c632945c6c053e6b495
kradford7 <kevin.radford117@gmail.com>
2022-03-14 10:01:06 -0700

    Added files for heroku

---
+++ init.R
@@ -0,0 +1,17 @@
+# R script to run author supplied code, typically used to install additional R packages
+# contains placeholders which are inserted by the compile script
+# NOTE: this script is executed in the chroot context; check paths!
+
+r <- getOption('repos')
+r['CRAN'] <- 'http://cloud.r-project.org'
+options(repos=r)
+
+# ======================================================================
+
+# packages go here
+install.packages('remotes')
+install.packages('dash')
+install.packages('plotly')
+install.packages('ggplot2')
+
+remotes::install_github('plotly/dashR', upgrade=TRUE)
