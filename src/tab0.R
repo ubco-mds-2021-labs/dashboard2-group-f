95a37ea9907ab71bf6e128595eaaa26e9031711e
Energix11 <monicamd@student.ubc.ca>
2022-03-16 11:45:59 -0700

    Basic landing page information

--- src/tab0.R
+++ src/tab0.R
@@ -1,1 +1,25 @@
-tab0 <- 'Welcome!'
+tab0 <- div(
+    h1(
+	'Welcome',
+	style = list(
+        'color' = 'var(--bs-primary)',
+        'textAlign' = 'center'
+        )
+        ),
+     h3(
+         'Surgical systems are a fundamental part of an effective health care system.'
+         ),
+     h4(
+         'Data related to surgical system functioning is collected, synthesized and published in the BC Data Catalogue and displayed here by total values across time and by hospital and procedure from 2009 to the most recent fiscal quarter'
+         ),
+     h4(
+         'Data for the counts of waiting and completed surgical cases is presented in the first tab to the left'
+         ),
+     h4(
+         'Data for the average wait times for completed surgical cases is presented in the second tab to the left'
+         ),
+     h4(
+         'Individual health authoritise can be selected from the dropdown menu.'
+     )
+
+)
